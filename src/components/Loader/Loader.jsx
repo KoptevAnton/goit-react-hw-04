780bc79dadf567567fdfb85a56485b824023d1fe
KoptevAnton <koptev110593@gmail.com>
2024-09-04 09:15:38 +0300

    Change size of icon.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,12 @@
 import { InfinitySpin } from 'react-loader-spinner';
-import css from './loader.module.css';
+import css from './Loader.module.css';
 
 export default function Loader() {
   return (
     <div className={css.container}>
       <InfinitySpin
         visible={true}
-        width="190"
+        width="200"
         color="rgb(89, 89, 236)"
         ariaLabel="infinity-spin-loading"
       />
